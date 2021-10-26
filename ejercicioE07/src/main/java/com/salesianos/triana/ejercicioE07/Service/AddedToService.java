3b7052e5aa0baa4e7f06eb0a6b17f2100d8354a6
javi navarro <tu@ejemplo.comlobosaur22@gmail.com>
2021-10-26 20:56:32 +0200

    sigue fallando

--- ejercicioE07/src/main/java/com/salesianos/triana/ejercicioE07/Service/AddedToService.java
+++ ejercicioE07/src/main/java/com/salesianos/triana/ejercicioE07/Service/AddedToService.java
@@ -2,10 +2,18 @@ package com.salesianos.triana.ejercicioE07.Service;
 
 import com.salesianos.triana.ejercicioE07.Model.AddedTo;
 import com.salesianos.triana.ejercicioE07.Model.AddedToPK;
+import com.salesianos.triana.ejercicioE07.Model.Playlist;
+import com.salesianos.triana.ejercicioE07.Model.Song;
 import com.salesianos.triana.ejercicioE07.Repository.AddedToRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class AddedToService extends BaseService<AddedTo, AddedToPK, AddedToRepository> {
+        public void createAddedTo (Playlist p , Song s , SongService ss , PlaylistService ps){
+            AddedTo addedTo = AddedTo.builder().orden(5).dateTime(LocalDateTime.now()).build();
 
+            addedTo.addSongPlaylist(s,p);
+        }
     }
